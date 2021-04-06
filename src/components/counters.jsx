be75cc64191bd08c50634f566fc3f6e58a7263ac
cbermedina <cbermedina@gmail.com>
2021-04-06 16:19:47 -0500

    Multiple components in Sync

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,19 +10,40 @@ class Counters extends Component {
             { id:4, value:4 },
             { id:5, value:5 }
         ]
+     };
+
+     handlerIncrement = counter =>{
+       const counters = [...this.state.counters];
+       const index = counters.indexOf(counter);
+       counters[index]= {...counter};
+       counters[index].value++;
+       this.setState({counters});
      }
+
+     handleReset = () => {
+      const counters = this.state.counters.map( c => {
+          c.value = 0;
+          return c;
+      });
+      this.setState({counters});
+     };
+
      handleDelete = counterId => {
        console.log('Event handler delete called', counterId);
        const counters = this.state.counters.filter(c => c.id !== counterId);   
        this.setState({counters});
-     }
+     };
 
     render() { 
         return (<div>
+            <button 
+            onclick={this.handleReset}
+            className="btn btn-primary btn-sm m-2">Reset</button>
             {this.state.counters.map(counter =>(
                 <Counter 
                    key={counter.id} 
                    onDelete={this.handleDelete} 
+                   onIncrement={this.handlerIncrement}
                    counter={counter}>
                   <h4>Counter #{counter.id}</h4>
                 </Counter>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counters from './components/counters'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
