130af54edf799162b1b6e9c3814a6075f602ec92
cbermedina <cbermedina@gmail.com>
2021-04-07 07:45:59 -0500

    Updatimg Phase

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,12 @@ class Counter  extends Component {
         tags: ['tag1','tag2','tag3']
     };
 
+    componentDidUpdate(prevProps, prevState){
+        console.log('Counter - DidUpdate');
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+    }
+
     renderTags(){
        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>)} </ul>;
